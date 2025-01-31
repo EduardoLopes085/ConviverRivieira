dde20b84c1b24be9c4c9e188366f5e5bb3846b7c
Eduardo Lopes <eduardolcb18@gmail.com>
2025-01-31 16:26:26 -0300

    conviver rivieira

---
+++ JavaScript/fancybox.js
@@ -0,0 +1,17 @@
+Fancybox.bind('[data-fancybox="gallery"]', {
+    Toolbar: {
+      display: {
+        left: ["infobar"],
+        middle: [
+          "zoomIn",
+          "zoomOut",
+          "toggle1to1",
+          "rotateCCW",
+          "rotateCW",
+          "flipX",
+          "flipY",
+        ],
+        right: ["slideshow", "thumbs", "close"],
+      },
+    },
+  });    
---
+++ JavaScript/script.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
+
+
+document.querySelectorAll('.overlay').forEach(function(overlay) {
+  overlay.textContent = overlay.getAttribute('data-text');
+});
+
+
+
+
+
+
+
